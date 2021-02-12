5d9b058d5fa37426717bdac54787d77884264099
Aditi Anmol <aditi.jeni@gmail.com>
2021-02-12 22:06:32 +0530

    passing-data-using-props

---
+++ src/IssueList.jsx
@@ -0,0 +1,66 @@
+class IssueRow extends React.Component {
+    render() {
+        const style = this.props.rowStyle;
+        return (
+            <tr>
+                <td style={style}>{this.props.issue_id}</td>
+                <td style={style}>{this.props.issue_title}</td>
+            </tr>
+        )
+    }
+}
+
+class IssueFilter extends React.Component {
+    render() {
+        return(
+            <div>This is the placeholder for Issue Filers.</div>
+        );
+    }
+}
+
+class IssueTable extends React.Component {
+    render() {
+        const rowStyle = { border: "1px solid silver", padding: 4 };
+        return(
+            <table style={{borderCollapse: "collapse"}}>
+                <thead>
+                    <tr>
+                        <th style={rowStyle}>ID</th>
+                        <th style={rowStyle}>Title</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <IssueRow rowStyle={rowStyle} issue_id={1} issue_title="Error on console when clicking Add"/>
+                    <IssueRow rowStyle={rowStyle} issue_id={2} issue_title="Missing bottom border on panel"/>
+                </tbody>
+                {/* <div>This is the placeholder for table of issues.</div> */}
+            </table>
+        );
+    }
+}
+
+class IssueAdd extends React.Component {
+    render() {
+        return(
+            <div>This is the placeholder for a form to add a new issue.</div>
+        );
+    }
+}
+
+class IssueList extends React.Component {
+    render() {
+        return(
+            <React.Fragment>
+                <h1>Issue Tracker</h1>
+                <IssueFilter />
+                <hr />
+                <IssueTable />
+                <hr />
+                <IssueAdd />
+            </React.Fragment>
+        );
+    }
+}
+
+const issueTrackerElem = document.getElementById('issuetracker');
+ReactDOM.render( <IssueList />, issueTrackerElem);

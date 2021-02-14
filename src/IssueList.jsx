c1941d7505016b8aee7e2cb3609cf0979678de3a
Aditi Anmol <aditi.jeni@gmail.com>
2021-02-14 10:31:56 +0530

    stateless-components

--- src/IssueList.jsx
+++ src/IssueList.jsx
@@ -8,22 +8,20 @@ const initialIssues = [
 ];
 
 
-class IssueRow extends React.Component {
-    render() {
-        const issue = this.props.issue;
-        console.log(issue);
-        return (
-            <tr>
-                <td>{issue.id}</td>
-                <td>{issue.status}</td>
-                <td>{issue.owner}</td>
-                <td>{issue.effort}</td>
-                <td>{issue.created.toDateString()}</td>
-                <td>{issue.due ? issue.due.toDateString() : ''}</td>
-                <td>{issue.title}</td>
-            </tr>
-        )
-    }
+function IssueRow(props) {
+    const issue = props.issue;
+    console.log(issue);
+    return (
+        <tr>
+            <td>{issue.id}</td>
+            <td>{issue.status}</td>
+            <td>{issue.owner}</td>
+            <td>{issue.effort}</td>
+            <td>{issue.created.toDateString()}</td>
+            <td>{issue.due ? issue.due.toDateString() : ''}</td>
+            <td>{issue.title}</td>
+        </tr>
+    )
 }
 
 class IssueFilter extends React.Component {
@@ -34,28 +32,26 @@ class IssueFilter extends React.Component {
     }
 }
 
-class IssueTable extends React.Component {
-    render() {
-        const issueRows = this.props.issues.map(issue => <IssueRow key={issue.id} issue={issue}/>);
-        return(
-            <table className="bordered-table">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Status</th>
-                        <th>Owner</th>
-                        <th>Created</th>
-                        <th>Effort</th>
-                        <th>Due Date</th>
-                        <th>Title</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {issueRows}
-                </tbody>
-            </table>
-        );
-    }
+function IssueTable(props) {
+    const issueRows = props.issues.map(issue => <IssueRow key={issue.id} issue={issue}/>);
+    return(
+        <table className="bordered-table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Status</th>
+                    <th>Owner</th>
+                    <th>Created</th>
+                    <th>Effort</th>
+                    <th>Due Date</th>
+                    <th>Title</th>
+                </tr>
+            </thead>
+            <tbody>
+                {issueRows}
+            </tbody>
+        </table>
+    );
 }
 
 class IssueAdd extends React.Component {

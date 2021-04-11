5b20e8d694a5c4504ab4b40e00f12c0853ae69bd
Aditi Anmol <aditi.jeni@gmail.com>
2021-04-11 19:31:19 +0530

    added delete api and impl

--- ui/src/IssueTable.jsx
+++ ui/src/IssueTable.jsx
@@ -1,26 +1,47 @@
 import React from 'react';
 import { Link, NavLink, withRouter } from 'react-router-dom';
 
-const IssueRow = withRouter(({ issue, location: { search } }) => {
+const IssueRow = withRouter(({
+  issue, location: { search }, closeIssue, deleteIssue, index,
+}) => {
   const selectLocation = { pathname: `/issues/${issue.id}`, search };
   return (
     <tr>
       <td>{issue.id}</td>
       <td>{issue.status}</td>
       <td>{issue.owner}</td>
-      <td>{issue.effort}</td>
       <td>{issue.created.toDateString()}</td>
+      <td>{issue.effort}</td>
       <td>{issue.due ? issue.due.toDateString() : ''}</td>
       <td>{issue.title}</td>
-      <td><Link to={`/edit/${issue.id}`}>Edit</Link>
+      <td>
+        <Link to={`/edit/${issue.id}`}>Edit</Link>
         {' | '}
-        <NavLink to={selectLocation}>Select</NavLink></td>
+        <NavLink to={selectLocation}>Select</NavLink>
+        {' | '}
+        <button type="button" onClick={() => { closeIssue(index); }}>
+          Close
+        </button>
+        {' | '}
+        <button type="button" onClick={() => { deleteIssue(index); }}>
+          Delete
+        </button>
+      </td>
     </tr>
   );
 });
 
-function IssueTable({ issues }) {
-  const issueRows = issues.map(issue => <IssueRow key={issue.id} issue={issue} />);
+export default function IssueTable({ issues, closeIssue, deleteIssue }) {
+  const issueRows = issues.map((issue, index) => (
+    <IssueRow
+      key={issue.id}
+      issue={issue}
+      closeIssue={closeIssue}
+      deleteIssue={deleteIssue}
+      index={index}
+    />
+  ));
+
   return (
     <table className="bordered-table">
       <thead>
@@ -41,5 +62,3 @@ function IssueTable({ issues }) {
     </table>
   );
 }
-
-export default IssueTable;
